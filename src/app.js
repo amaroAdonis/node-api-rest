6ee5140d7157c323cc229ce6de830c95e8617919
Amaro Adonis Moraes Amorim <amaro.adonis@gmail.com>
2024-04-25 22:25:46 -0300

    routs post and delete

--- src/app.js
+++ src/app.js
@@ -7,10 +7,17 @@ app.use(express.json())
 const selecoes = [
     {id: 1, selecao: 'Brasil', grupo: 'G'},
     {id: 2, selecao: 'Suiça', grupo: 'G'},
-    {id: 3, selecao: 'Sérvia', grupo: 'G'},
-    {id: 4, selecao: 'Camarões', grupo: 'G'},
+    {id: 3, selecao: 'Camarões', grupo: 'G'},
+    {id: 4, selecao: 'Sérvia', grupo: 'G'},
 ]
 
+function buscarSelecaoPorId(id) {
+    return selecoes.filter(selecao => selecao.id == id)
+}
+
+function buscaIndexSelecao(id) {
+    return selecoes.findIndex( selecao => selecao.id == id)
+}
 
 
 //rota padrão
@@ -24,9 +31,20 @@ app.get('/selecoes', (req, res) => {
     res.status(200).send(selecoes)
 })
 
+app.get('/selecoes/:id', (req, res) => {
+    res.json(buscarSelecaoPorId(req.params.id))
+})
+
 app.post('/selecoes', (req, res) => {
     selecoes.push(req.body)
     res.status(201).send('Seleção cadastrada com sucesso')
 })
+
+app.delete('/selecoes/:id', (req, res) => {
+    let index = buscaIndexSelecao(req.params.id)
+    selecoes.splice(index, 1)
+    res.send(`Seleção com o id ${req.params.id}, excluída com sucesso!`)
+
+} )
 
 export default app
